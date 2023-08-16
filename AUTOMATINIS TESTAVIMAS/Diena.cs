951dc3eaf5fde32753eeb8b0b77b7a8878ff82e0
veiksmas <autocadbim3@gmail.com>
2023-08-16 16:46:37 +0100

    pirmas bandymas

---
+++ AUTOMATINIS TESTAVIMAS/Diena.cs
@@ -0,0 +1,25 @@
+﻿
+
+using NUnit.Framework;
+
+namespace AUTOMATINIS_TESTAVIMAS
+{
+    internal class Diena
+    {
+
+        public void naujas()
+
+        {
+            int sk1 = 6;
+            int sk2 = 8;
+            int expectedResult = 14;
+
+            int actualResult = sk1 + sk2;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+    }
+
+}
+
