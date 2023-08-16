f4b04d85d437ee5ae772718c742dafefb755c8c3
veiksmas <autocadbim3@gmail.com>
2023-08-16 15:22:12 +0100

    Add project files.

---
+++ AUTOMATINIS TESTAVIMAS/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AUTOMATINIS_TESTAVIMAS
+{
+    public class Class1
+    {
+    }
+}
